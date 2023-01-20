8c849ef9ad5f9761849004569065149c0d97d61f
Filip Lobpreis <f.lobpreis@gmail.com>
2023-01-20 23:11:15 +0100

    sfml: load hero and surroundings
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/sfmlBasics.cpp
+++ src/sfmlBasics.cpp
@@ -5,16 +5,30 @@ sf::VideoMode vm(viewSize.x, viewSize.y);
 sf::RenderWindow window(vm, "Hello world", sf::Style::Default);
 
 sf::Texture skyTexture;
+sf::Texture bgTexture;
+sf::Texture heroTexture;
 sf::Sprite skySprite;
+sf::Sprite bgSprite;
+sf::Sprite heroSprite;
 
 void init()
 {
 	skyTexture.loadFromFile("../Assets/graphics/sky.png");
 	skySprite.setTexture(skyTexture);
+
+	bgTexture.loadFromFile("../Assets/graphics/bg.png");
+	bgSprite.setTexture(bgTexture);
+
+	heroTexture.loadFromFile("../Assets/graphics/hero.png");
+	heroSprite.setTexture(heroTexture);
+	heroSprite.setPosition(viewSize.x/2, viewSize.y/2);
+	heroSprite.setOrigin(heroTexture.getSize().x/2., heroTexture.getSize().y/2.);
 }
 
 void draw()
 {
 	window.draw(skySprite);
+	window.draw(bgSprite);
+	window.draw(heroSprite);
 }
 
